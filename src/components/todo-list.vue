b34e27050b8a4338af95a402b4297aec5a2fa20a
Mohamed Ghanem <ma-ghanem@hotmail.com>
2020-01-10 19:21:47 +0200

    Dynamically display the todo-list component from the store

--- src/components/todo-list.vue
+++ src/components/todo-list.vue
@@ -1,18 +1,25 @@
 <template lang="html">
   <div>
-    <TodoItem class="mb-3" />
-    <TodoItem class="mb-3"/>
+    <TodoItem
+      class="mb-3"
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"/>
   </div>
 </template>
 
 <script>
-import TodoItem from '@/components/todo-item';
+import TodoItem from "@/components/todo-item";
+import { mapState } from "vuex";
 
 export default {
+  computed: mapState({
+    todos: state => state.todos.todos
+  }),
   components: {
-    TodoItem,
+    TodoItem
   }
-}
+};
 </script>
 
 <style lang="css">
