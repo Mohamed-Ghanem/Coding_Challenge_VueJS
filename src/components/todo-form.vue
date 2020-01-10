b8fbd93ddf8fc5b6c51a55c64173280e007cb3b9
Mohamed Ghanem <ma-ghanem@hotmail.com>
2020-01-10 19:20:53 +0200

    Implement CRUD features and integrate with the store

--- src/components/todo-form.vue
+++ src/components/todo-form.vue
@@ -1,14 +1,33 @@
 <template lang="html">
-  <v-text-field
-    label="Todo"
-    placeholder="What do you have to do?"
-    solo
-  />
+  <form @submit.prevent="addTodo">
+    <v-text-field
+      label="Todo"
+      placeholder="What do you have to do?"
+      solo
+      v-model="title"
+    />
+  </form>
 </template>
 
 <script>
+import store from "@/store/store";
+import * as type from "@/actions";
+
 export default {
-}
+  data() {
+    return {
+      title: "",
+      newId: 0
+    };
+  },
+  methods: {
+    addTodo() {
+      store.dispatch(type.addTodo, this);
+      this.newId++;
+      this.title = "";
+    }
+  }
+};
 </script>
 
 <style lang="css">
--- src/components/todo-item.vue
+++ src/components/todo-item.vue
@@ -2,22 +2,48 @@
   <v-card>
     <v-card-title primary-title>
       <div>
-        <h3 class="headline mb-0">Title</h3>
+        <h3
+          class="headline mb-0"
+          :class="{'completed': todo.completed}">
+          {{ todo.title }}
+        </h3>
       </div>
     </v-card-title>
-    <v-btn>
-      Complete
+    <v-btn @click="toggleTodo()">
+      {{ todo.completed ? 'UnComplete': 'Complete' }}
     </v-btn>
-    <v-btn color="error">
+    <v-btn
+      color="error"
+      @click="deleteTodo()">
       Delete
     </v-btn>
   </v-card>
 </template>
 
 <script>
+import store from "@/store/store";
+import * as type from "@/actions";
+
 export default {
-}
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    toggleTodo() {
+      store.dispatch(type.toggleTodo, this.todo.id);
+    },
+    deleteTodo() {
+      store.dispatch(type.deleteTodo, this.todo.id);
+    }
+  }
+};
 </script>
 
 <style lang="css">
+.completed {
+  text-decoration: line-through;
+}
 </style>
